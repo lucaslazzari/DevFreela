0534dec81a20ec58794503b810b746182966b92d
lucaslazzari <lucaslazzari1812@gmail.com>
2024-02-08 14:20:25 -0300

    tirando namespaces que nao estavam sendo usados para limpar o codigo

---
+++ DevFreela/DevFreela.UnitTests/Application/Commands/FinishProjectCommandHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class FinishProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectFinished()
+        {
+        }
+    }
+}
